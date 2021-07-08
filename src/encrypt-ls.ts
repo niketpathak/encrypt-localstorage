f5077fbf8cfefe91c965fd876b38a3cfa35e1d7c
Niket Pathak <niket.pathak@radiofrance.com>
2021-07-08 14:23:24 +0200

    chore: optimize encrypter/decrypter methods

--- src/encrypt-ls.ts
+++ src/encrypt-ls.ts
@@ -8,14 +8,9 @@ import ls from 'localstorage-slim';
 import AES from 'crypto-js/aes';
 import encUTF8 from 'crypto-js/enc-utf8';
 
-ls.config.encrypter = (data: unknown, secret: string) => {
-  const encrypted = AES.encrypt(JSON.stringify(data), secret);
-  return encrypted.toString();
-};
-ls.config.decrypter = (data: string, secret: string) => {
-  const decrypted = AES.decrypt(data, secret);
-  return JSON.parse(decrypted.toString(encUTF8));
-};
+ls.config.encrypter = (data: unknown, secret: string) => AES.encrypt(JSON.stringify(data), secret).toString();
+ls.config.decrypter = (data: unknown, secret: string) =>
+  JSON.parse(AES.decrypt(data as string, secret).toString(encUTF8));
 
 ls.config.encrypt = true;
 ls.config.secret = 'secret-string';
