b42dcf7904f7b3283848c174765b003e4cfb9460
Niket Pathak <niket.pathak@radiofrance.com>
2021-07-08 12:49:32 +0200

    ✨⚡️🔥 feat: encrypt localstorage key with AES encryption

--- src/encrypt-ls.ts
+++ src/encrypt-ls.ts
@@ -4,8 +4,28 @@
  * MIT License
  */
 
-import { isObject, NOOP } from './helpers';
 import ls from 'localstorage-slim';
-import sha256 from 'crypto-js/sha256';
+import AES from 'crypto-js/aes';
+import encUTF8 from 'crypto-js/enc-utf8';
+
+ls.config.encrypter = (data: unknown, secret: string) => {
+  const encrypted = AES.encrypt(JSON.stringify(data), secret);
+  return encrypted.toString();
+};
+ls.config.decrypter = (data: string, secret: string) => {
+  const decrypted = AES.decrypt(data, secret);
+  return JSON.parse(decrypted.toString(encUTF8));
+};
+
+ls.config.encrypt = true;
+ls.config.secret = 'secret-string';
+
+ls.set('number', 25865);
+ls.set('text', 'hello world');
+ls.set('important-token', { ching: 'faa', rabbi: { rabbit: 'animal' } }, { ttl: 10 });
+
+console.log('ls.get("number") :>> ', ls.get('number'));
+console.log('ls.get("text") :>> ', ls.get('text'));
+console.log('ls.get("important-token") :>> ', ls.get('important-token'));
 
 export const app = {};
